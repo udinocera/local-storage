d454e77b217f086db43ea4411894c25877c9b103
udinocera <udinocera@gmail.com>
2023-09-16 16:36:06 +0200

    fine

--- exercise.js
+++ exercise.js
@@ -3,3 +3,15 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function saveUser(data) {
+  try {
+    const userJSON = JSON.stringify(data);
+    localStorage.setItem("user", userJSON);
+    console.log("User salvato nel localStorage.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+saveUser(user);
